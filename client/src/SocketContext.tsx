1b649f53a5ef6972399125bec9359677dcb0d0b5
Josh Whitney <josh.whitney@punchcard.io>
2022-10-27 09:37:32 -0600

    Environment changes

--- client/src/SocketContext.tsx
+++ client/src/SocketContext.tsx
@@ -2,5 +2,5 @@ import { createContext } from "react";
 import io from "socket.io-client";
 
 console.log('url', process.env.REACT_APP_SERVER_URL);
-export const socket = io(process.env.REACT_APP_SERVER_URL || "https://catchphrase-app-797gz.ondigitalocean.app");
+export const socket = io(process.env.REACT_APP_SERVER_URL || "");
 export const SocketContext = createContext(socket);
